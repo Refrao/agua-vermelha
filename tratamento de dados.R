254480017920558affbd9f8198da26d28a6bd738
Refrao <94881413+Refrao@users.noreply.github.com>
2021-11-27 23:05:38 -0300

    v4

--- tratamento de dados.R
+++ tratamento de dados.R
@@ -1,7 +1,10 @@
 library(tidyverse)
 
+#Ler arquivo
+
 phs <- read_csv("~/Estudos de R/phs_2021_1.csv")
 
+#alterar 
 phs$stat_amount <- round(phs$stat_amount,2)
 
 tabela_stats <- phs %>% select(stat_name,team_name, hero_name, stat_amount) %>% 
@@ -42,3 +45,25 @@ library(plotly)
 ggplotly(
 tfinal %>% ggplot(mapping = aes(x = qtdt, y=somt, size = qtd)) + geom_point(aplha = 0.5, colour = "blue"))
 )
+
+
+phs <- phs %>% separate(start_time, into= c("data", "hora"), sep = " ") 
+
+phs$data <- as.Date(phs$data)
+
+library(janitor)
+
+phs_1 <- phs %>% filter(hero_name != "All Heroes") %>% 
+                 group_by(data, map_name) %>% 
+                 summarise(soma = sum(stat_amount)) %>% 
+                 ungroup() %>%  droplevels(.) %>% 
+                 pivot_wider(names_from = map_name, values_from = soma) %>% 
+                 adorn_totals()
+                 
+
+
+
+
+        
+
+
